ff1d9488b2e182747f8a56ec22cf84909c393e38
Matthew Higgins <mthiggins1s@semo.edu>
2025-05-01 15:45:42 -0500

    Add Machu Picchu to user places and refactor addPlaceToUserPlaces method

--- 01-starting-project/src/app/places/available-places/available-places.component.ts
+++ 01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -50,7 +50,7 @@ export class AvailablePlacesComponent implements OnInit{
   }
 
   onSelectPlace(selectedPlace: Place) {
-    const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace.id)
+    const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace)
     .subscribe({
       next: (resData) => console.log(resData),
     });
--- 01-starting-project/src/app/places/user-places/user-places.component.ts
+++ 01-starting-project/src/app/places/user-places/user-places.component.ts
@@ -1,8 +1,6 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
-import { HttpClient } from '@angular/common/http';
-import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 
 @Component({
@@ -13,19 +11,15 @@ import { PlacesService } from '../places.service';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent implements OnInit {
-  places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
-
   private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
+  places = this.placesService.loadedUserPlaces;
 
   ngOnInit() {
     this.isFetching.set(true);
     const subscription = this.placesService.loadUserPlaces().subscribe({
-      next: (places) => {
-        this.places.set(places);
-      },
       error: (error: Error) => {
         console.log(error);
         this.error.set(error.message);
